ac87bddebc600409c329314024ba0987b40bc230
Smriti <smritivashisth@gmail.com>
2015-04-02 02:53:02 +0530

    add datepicker implementation

--- ResumeBuilder/src/com/example/resumebuilder/PersonalDetailsFragment.java
+++ ResumeBuilder/src/com/example/resumebuilder/PersonalDetailsFragment.java
@@ -1,21 +1,34 @@
 package com.example.resumebuilder;
 
 import android.annotation.SuppressLint;
+import android.app.DialogFragment;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 @SuppressLint("NewApi")
-public class PersonalDetailsFragment extends Fragment {
+public class PersonalDetailsFragment extends Fragment implements
+		OnClickListener {
 
 	String[] genderArr = { "Male", "Female", "Other" };
 	String[] categoryArr = { "General", "OBC", "SC/ST" };
+	int year, month, day;
+	String date;
 
 	Spinner genderSpinner, categorySpinner;
+	EditText etName, etEmail, etContact, etEmployment, etFather, etPermanent,
+			etCurrent;
+	ImageButton imgBtnDate;
+	Button btnPersonalDetails;
 
 	public PersonalDetailsFragment() {
 	}
@@ -27,6 +40,18 @@ public class PersonalDetailsFragment extends Fragment {
 		View rootView = inflater.inflate(R.layout.personaldetails, container,
 				false);
 
+		etName = (EditText) rootView.findViewById(R.id.etName);
+		etEmail = (EditText) rootView.findViewById(R.id.etEmail);
+		etContact = (EditText) rootView.findViewById(R.id.etContact);
+		etEmployment = (EditText) rootView.findViewById(R.id.etEmployment);
+		etFather = (EditText) rootView.findViewById(R.id.etFather);
+		etPermanent = (EditText) rootView.findViewById(R.id.etPermanent);
+		etCurrent = (EditText) rootView.findViewById(R.id.etCurrent);
+		imgBtnDate = (ImageButton) rootView.findViewById(R.id.imgBtnDate);
+		btnPersonalDetails = (Button) rootView
+				.findViewById(R.id.btnPersonalDetails);
+
+		
 		genderSpinner = (Spinner) rootView.findViewById(R.id.gender_spinner);
 		ArrayAdapter<String> adapter_gender = new ArrayAdapter<String>(
 				getActivity(), android.R.layout.simple_spinner_item, genderArr);
@@ -34,6 +59,7 @@ public class PersonalDetailsFragment extends Fragment {
 				.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		genderSpinner.setAdapter(adapter_gender);
 
+		
 		categorySpinner = (Spinner) rootView
 				.findViewById(R.id.category_spinner);
 		ArrayAdapter<String> adapter_category = new ArrayAdapter<String>(
@@ -43,6 +69,27 @@ public class PersonalDetailsFragment extends Fragment {
 				.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		categorySpinner.setAdapter(adapter_category);
 
+		
+		imgBtnDate.setOnClickListener(this);
+		btnPersonalDetails.setOnClickListener(this);
+
 		return rootView;
 	}
+
+	@Override
+	public void onClick(View view) {
+		// TODO Auto-generated method stub
+		switch (view.getId()) {
+		case R.id.imgBtnDate:
+			Toast.makeText(getActivity(), "Image button clicked",
+					Toast.LENGTH_LONG).show();
+			DialogFragment picker = new DatePickerFragment();
+			picker.show(getFragmentManager(), "datePicker");
+			break;
+		case R.id.btnPersonalDetails:
+			break;
+		}
+
+	}
+
 }
