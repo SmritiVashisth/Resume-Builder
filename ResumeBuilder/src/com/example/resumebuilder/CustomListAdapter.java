2501fdc95978f2eba31b8c9eff1b7412b4b9d044
Smriti <smritivashisth@gmail.com>
2015-04-04 15:13:51 +0530

    custom listview layouts

---
+++ ResumeBuilder/src/com/example/resumebuilder/CustomListAdapter.java
@@ -0,0 +1,51 @@
+package com.example.resumebuilder;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+public class CustomListAdapter extends BaseAdapter {
+
+	ArrayList<String> list;
+	private LayoutInflater inflater;
+	Activity activity;
+
+	public CustomListAdapter(Activity activity) {
+		this.activity = activity;
+	}
+
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Object getItem(int position) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long getItemId(int position) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		// TODO Auto-generated method stub
+		if (inflater == null)
+			inflater = (LayoutInflater) activity
+					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		if (convertView == null)
+			convertView = inflater.inflate(R.layout.customlistview, null);
+		return convertView;
+	}
+
+}
---
+++ ResumeBuilder/src/com/example/resumebuilder/EduListAdapter.java
@@ -0,0 +1,48 @@
+package com.example.resumebuilder;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+public class EduListAdapter extends BaseAdapter {
+
+	private LayoutInflater inflater;
+	Activity activity;
+
+	public EduListAdapter(Activity activity) {
+		this.activity = activity;
+	}
+
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Object getItem(int position) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long getItemId(int position) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		// TODO Auto-generated method stub
+		if (inflater == null)
+			inflater = (LayoutInflater) activity
+					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		if (convertView == null)
+			convertView = inflater.inflate(R.layout.edulistitem, null);
+		return convertView;
+	}
+
+}
