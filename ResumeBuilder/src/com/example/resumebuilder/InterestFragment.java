a66f78b12b590883714a0436d2a14f9c49cc4b9c
Smriti <smritivashisth@gmail.com>
2015-04-05 13:19:01 +0530

    implement working of interest fragment

--- ResumeBuilder/src/com/example/resumebuilder/InterestFragment.java
+++ ResumeBuilder/src/com/example/resumebuilder/InterestFragment.java
@@ -1,14 +1,28 @@
 package com.example.resumebuilder;
 
+import java.util.ArrayList;
+
 import android.annotation.SuppressLint;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 @SuppressLint("NewApi")
-public class InterestFragment extends Fragment {
+public class InterestFragment extends Fragment implements OnClickListener {
+
+	ListView lvInterest;
+	EditText etInterest;
+	Button btnAddInterest, btnSaveInterest;
+	ArrayList<String> interests = new ArrayList<String>();
+	ArrayAdapter<String> adapter;
+
 	public InterestFragment() {
 	}
 
@@ -16,10 +30,47 @@ public class InterestFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 
-		View rootView = inflater.inflate(R.layout.interest, container,
-				false);
+		View rootView = inflater.inflate(R.layout.interest, container, false);
+
+		lvInterest = (ListView) rootView.findViewById(R.id.lvInterests);
+		etInterest = (EditText) rootView.findViewById(R.id.etInterest);
+		btnAddInterest = (Button) rootView.findViewById(R.id.btnAddInterest);
+		btnSaveInterest = (Button) rootView.findViewById(R.id.btnSaveInterest);
+
+		etInterest.setText(null);
+		btnAddInterest.setOnClickListener(this);
+		btnSaveInterest.setOnClickListener(this);
+
+		adapter = new ArrayAdapter<String>(getActivity(),
+				android.R.layout.simple_list_item_1, interests);
+		lvInterest.setAdapter(adapter);
 
 		return rootView;
+	}
+
+	@Override
+	public void onClick(View view) {
+		// TODO Auto-generated method stub
+		switch (view.getId()) {
+		case R.id.btnAddInterest:
+			String str = etInterest.getText().toString();
+			if (str.length() > 0) {
+				interests.add(str);
+				etInterest.setText(null);
+				adapter.notifyDataSetChanged();
+				// reload the fragment
+				// intent = new Intent(getActivity(), InterestFragment.class);
+				// startActivity(intent);
+			}
+			break;
+		case R.id.btnSaveInterest:
+			// save to database
+			// reload fragment
+			adapter.notifyDataSetChanged();
+			btnAddInterest.setVisibility(View.INVISIBLE);
+			etInterest.setVisibility(View.INVISIBLE);
+			break;
+		}
 	}
 
 }
